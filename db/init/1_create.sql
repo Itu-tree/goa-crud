4c03ddaf728e2dc4342a19a6a2abc3ebcb2feefe
toutou <toutounode@gmail.com>
2022-05-06 21:31:17 +0900

    golang-apiからアクセスする用のユーザーを追加

--- db/init/1_create.sql
+++ db/init/1_create.sql
@@ -12,3 +12,10 @@ CREATE TABLE users (
 -- usersテーブルに２つレコードを追加
 INSERT INTO users (name, password) VALUES ("gophar", "5555");
 INSERT INTO users (name, password) VALUES ("octcat", "0000");
+
+-- golagn-api用のユーザーを作成
+CREATE USER 'golang-api'@'%' IDENTIFIED BY 'password';
+-- 認証方式を変更
+ALTER USER 'golang-api'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+-- 権限を追加(@の直後について、golang-api(docker-composeのサービス名)からのみアクセスできる)
+GRANT SELECT,INSERT,UPDATE,DELETE ON golang_db.* TO 'golang-api'@'%';
